30c394d944a57969924e23cfa15fa7a6ba6794bd
William <william@undefined.re>
2019-08-22 12:49:03 +0200

    WIP firmware rebuild

--- install-kernel.sh
+++ install-kernel.sh
@@ -11,6 +11,7 @@ curr_kv=linux-$(make kernelversion)
 latest_kv=$(eselect kernel list | sed -rne '$ s/^.*(linux-[^ ]+).*$/\1/p')
 pwd
 
+################################# kernel version:
 if [ $latest_kv != $curr_kv ]; then
   echo "$latest_kv != $curr_kv, fucking right off"
   exit 1
@@ -29,6 +30,32 @@ fi
 
 eselect kernel set $latest_kv
 
+################################# firmware:
+fm_file=$(ls -1 /usr/portage/distfiles/linux-firmware-* | sort | tail -n 1)
+
+# FIXME nostromo!
+case $HOST in
+  daban-urnud)
+    firmware='iwlwifi-7265D|i915'
+    #echo 'tart $tree/distfiles/linux-firmware-20170622.tar.gz | sed -nre "/(iwlwifi-7265D|i915)/ { s:^.*linux-firmware[^/]*/::g; s/ ->.*//g; p }" | sudo tee $portage/savedconfig/sys-kernel/linux-firmware && emerge -1 linux-firmware'
+    ;;
+  yggdrasill)
+    firmware='i915|ath10k|qca'
+    ###echo 'tart $tree/distfiles/linux-firmware-20181026.tar.gz | sed -nre "/(i915|ath10k|qca)/ { s:^.*linux-firmware[^/]*/::g; s/ ->.*//g; p }" | sudo tee $portage/savedconfig/sys-kernel/linux-firmware && emerge -1 linux-firmware'
+    ;;
+esac
+echo you might want to update firmware:
+echo $fm_file with ${firmware}?
+read ans
+
+if [[ ( "$ans" = "" || "$ans" = yes || "$ans" = y ) ]]; then
+  saved_config=/etc/portage/savedconfig/sys-kernel/linux-firmware
+  tart $fm_file | sed -nre "/($firmware)/ { s:^.*linux-firmware[^/]*/::g; s/ ->.*//g; p }" | tee $saved_config
+  chown portage:portage $saved_config
+  emerge -1 linux-firmware           || die
+fi
+
+################################# save config:
 cp .config /home/wjc/conf/kernel-config/$HOST/config-$curr_kr
 chown wjc:wjc /home/wjc/conf/kernel-config/$HOST/config-$curr_kr
 
@@ -37,19 +64,9 @@ mount /boot
 chown -RL portage:portage /usr/src/linux
 make modules_install                 || die
 make install                         || die
+
 emerge @module-rebuild               || die
 #genkernel --install --no-ramdisk-modules --firmware initramfs
 #genkernel --install --firmware initramfs
 dracut --hostonly '' $curr_kr        || die
 grub-mkconfig -o /boot/grub/grub.cfg || die
-
-# FIXME nostromo!
-echo you might want to update firmware:
-case $HOST in
-  daban-urnud)
-    echo 'tart $tree/distfiles/linux-firmware-20170622.tar.gz | sed -nre "/(iwlwifi-7265D|i915)/ { s:^.*linux-firmware[^/]*/::g; s/ ->.*//g; p }" | sudo tee $portage/savedconfig/sys-kernel/linux-firmware && emerge -1 linux-firmware'
-    ;;
-  yggdrasill)
-    echo 'tart $tree/distfiles/linux-firmware-20181026.tar.gz | sed -nre "/(i915|ath10k|qca)/ { s:^.*linux-firmware[^/]*/::g; s/ ->.*//g; p }" | sudo tee $portage/savedconfig/sys-kernel/linux-firmware && emerge -1 linux-firmware'
-    ;;
-esac
